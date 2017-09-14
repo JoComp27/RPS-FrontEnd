04a37158220dd49dbc3706289f79974f81c000f1
Refat <refat.kamal@scigilian.com>
2017-09-14 14:55:52 -0400

    I did mostly nothing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,12 @@ export class AppComponent {
     value = 50;
     bufferValue = 75;
 
-    tiles = [
+    tiles: Tile[] = [
         {text: 'Rock', cols: 2, rows: 1, color: 'red'},
         {text: 'Paper', cols: 2, rows: 1, color: 'grey'},
         {text: 'Scissors', cols: 2, rows: 1, color: 'royalblue'},
-        {text: 'Start', cols: 6, rows: 1, color: 'lightblue'}
-    ]
+       // {text: 'Start', cols: 6, rows: 1, color: 'lightblue'}
+    ];
 
     constructor(private http: Http) {
 
@@ -32,4 +32,15 @@ export class AppComponent {
 
     }
 
+    foo(tile: Tile): void {
+        console.log(tile.text);
+    }
+
+}
+
+interface Tile {
+    text: string;
+    cols: number;
+    rows: number;
+    color: string;
 }
