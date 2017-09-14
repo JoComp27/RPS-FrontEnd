00ec0c1596728ca6bc70f905247916e2aa496d91
Jonathan Cournoyer <jonathan.cournoyer@scigilian.com>
2017-09-14 11:25:52 -0400

    <Actual Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+    myData: Array<any>;
+    color = 'primary';
+    mode = 'determinate';
+    value = 50;
+    bufferValue = 75;
+
+    tiles = [
+        {text: 'Rock', cols: 2, rows: 1, color: 'red'},
+        {text: 'Paper', cols: 2, rows: 1, color: 'grey'},
+        {text: 'Scissors', cols: 2, rows: 1, color: 'royalblue'},
+        {text: 'Start', cols: 6, rows: 1, color: 'lightblue'}
+    ]
+
+    constructor(private http: Http) {
+
+
+        this.http.get('https://jsonplaceholder.typicode.com/photos')
+            .map(response => response.json())
+            .subscribe(res => this.myData = res);
+
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdGridListModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdMenuModule,
+        MdCardModule,
+        MdToolbarModule,
+        MdIconModule,
+        MdGridListModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
