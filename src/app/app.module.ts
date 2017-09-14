9e4173851b928cb3e90d8b120c132a91e412515e
Jonathan Cournoyer <jonathan.cournoyer@scigilian.com>
2017-09-14 15:58:05 -0400

    Created Enter Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdGridListModule, MdButtonToggleModule} from '@angular/material';
+import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdButtonToggleModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
@@ -22,7 +22,6 @@ import {HttpModule} from '@angular/http';
         MdCardModule,
         MdToolbarModule,
         MdIconModule,
-        MdGridListModule,
         MdButtonToggleModule
     ],
     providers: [],
